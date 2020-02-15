835948b7a8e043f72a5c7e4d992242240a59b20d
oheyy <daniel.seunghyun.oh@gmail.com>
2020-02-15 17:45:09 +1100

    Update test cases

--- src/com/daniel/codingexercises/challenges/SortArrayByParity/SortArrayByParityTest.java
+++ src/com/daniel/codingexercises/challenges/SortArrayByParity/SortArrayByParityTest.java
@@ -10,6 +10,11 @@ class SortArrayByParityTest {
     void sortArrayByParity() {
         SortArrayByParity sortArrayByParity = new SortArrayByParity();
         int[] A = {3,1,2,4};
-        sortArrayByParity.sortArrayByParity(A);
+        int[] result = sortArrayByParity.sortArrayByParity(A);
+        assertEquals(4, result.length);
+        int[] expectedArray = {2,4,1,3};
+        for(int i=0; i<expectedArray.length; i++){
+            assertEquals(expectedArray[i], result[i]);
+        }
     }
 }
